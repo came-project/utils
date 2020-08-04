9c655f3c345ce506759e78f68aa73c6d0c7f53fd
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-04 13:18:41 +0300

    added imageio

---
+++ include/imageio.h
@@ -0,0 +1,36 @@
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <vector>
+
+namespace utils {
+
+/**
+ * Reads an image from a stream
+ * 
+ * @param input Stream from which image is read
+ * @return image from `input` 
+ */
+cv::Mat read_image(std::istream & input = std::cin);
+
+/**
+ * Reads images from a stream until no characters are left
+ * or the stream fails
+ * 
+ * @param input Stream from which image is read
+ * @return vector of images from `input` 
+ */
+std::vector<cv::Mat> read_images();
+
+/**
+ * Write an image to a stream in pbm, pgm or pbm
+ * If `binary` is true, image is written in pbm,
+ * otherwise format is decided based on `image.channels()`
+ * 
+ * @param image Image to write
+ * @param input Stream to which image is written
+ * @param binary whether to write image as a binary one (pbm)
+ */
+void write_image(cv::Mat image, std::ostream & output = std::cout, bool binary = false);
+
+} // namespace utils
---
+++ src/imageio.cpp
@@ -0,0 +1,84 @@
+#include <imageio.h>
+
+namespace utils {
+
+cv::Mat read_image(std::istream& input)
+{
+    input >> std::noskipws;
+    std::vector<char> buf;
+    size_t newline_count = 0;
+    size_t prefix_size = 0;
+    bool reading_prefix = true;
+    size_t bit_per_pixel = 8;
+    size_t height;
+    size_t width;
+
+    while (!input.eof() && (reading_prefix || (buf.size() - prefix_size) * 8 < height * width * bit_per_pixel)) {
+        char t;
+        input >> t;
+        if (reading_prefix)
+            prefix_size++;
+        if (t == '\n') {
+            newline_count++;
+            if (newline_count == 2) {
+                std::stringstream ss(std::string(buf.begin() + 3, buf.end()));
+                ss >> height >> width;
+                if (buf[1] == '4') {
+                    bit_per_pixel = 1;
+                    reading_prefix = false;
+                }
+            }
+            if (newline_count == 3 && buf[1] != '4') {
+                if (buf[1] == '6') {
+                    bit_per_pixel = 24;
+                }
+                reading_prefix = false;
+            }
+        }
+        buf.push_back(t);
+    }
+
+    return cv::imdecode(buf, cv::IMREAD_UNCHANGED);
+}
+
+std::vector<cv::Mat> read_images()
+{
+    std::vector<cv::Mat> result;
+    while (!std::cin.eof()) {
+        cv::Mat image = read_image();
+        if (!image.data) {
+            break;
+        }
+        result.push_back(image);
+    }
+    return result;
+}
+
+void write_image(cv::Mat image, std::ostream & output, bool binary)
+{
+    std::vector<unsigned char> output_buffer;
+    std::string output_format;
+    if (binary) {
+        output_format = "pbm";
+    } else {
+        switch (image.channels())
+        {
+        case 1:
+            output_format = "pgm";
+            break;
+
+        case 3:
+            output_format = "ppm";
+            break;
+        
+        default:
+            throw std::domain_error(std::to_string(image.channels()) + " channels is not supported by netpbm");
+        }
+    }
+    cv::imencode(output_format, image, output_buffer);
+    for (unsigned char c : output_buffer) {
+        output << c;
+    }
+}
+
+} // namespace utils
