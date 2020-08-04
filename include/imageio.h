891fb34c45d9aa7d9dbdf3bc49db42cfa3e2901e
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-04 14:19:23 +0300

    docstring formatting fixes

--- include/imageio.h
+++ include/imageio.h
@@ -9,27 +9,26 @@ namespace utils {
  * Reads an image from a stream
  * 
  * @param input Stream from which image is read
- * @return image from `input` 
+ * @return Image from `input` 
  */
 cv::Mat read_image(std::istream & input = std::cin);
 
 /**
- * Reads images from a stream until no characters are left
- * or the stream fails
+ * Reads images from a stream until no characters are left or the stream fails
  * 
  * @param input Stream from which image is read
- * @return vector of images from `input` 
+ * @return Vector of images from `input` 
  */
 std::vector<cv::Mat> read_images();
 
 /**
- * Write an image to a stream in pbm, pgm or pbm
- * If `binary` is true, image is written in pbm,
+ * Write an image to a stream in pbm, pgm or pbm. 
+ * If `binary` is true, image is written in pbm, 
  * otherwise format is decided based on `image.channels()`
  * 
  * @param image Image to write
  * @param input Stream to which image is written
- * @param binary whether to write image as a binary one (pbm)
+ * @param binary Whether to write image as a binary one (pbm)
  */
 void write_image(cv::Mat image, std::ostream & output = std::cout, bool binary = false);
 
