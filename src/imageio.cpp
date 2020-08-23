ed27a1a008351622e7998d983e63042c2e2ce5db
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-23 12:25:09 +0300

    added 16-bit image support close #2

--- src/imageio.cpp
+++ src/imageio.cpp
@@ -19,6 +19,7 @@ cv::Mat read_image(std::istream & input, bool untie_input)
     size_t bit_per_pixel = 8;
     size_t height;
     size_t width;
+    size_t second_newline_idx;
 
     char t;
     while ((reading_prefix || (buf.size() - prefix_size) * 8 < height * width * bit_per_pixel) && input >> t) {
@@ -27,6 +28,7 @@ cv::Mat read_image(std::istream & input, bool untie_input)
         if (t == '\n') {
             newline_count++;
             if (newline_count == 2) {
+                second_newline_idx = buf.size();
                 std::stringstream ss(std::string(buf.begin() + 3, buf.end()));
                 ss >> height >> width;
                 if (buf[1] == '4') {
@@ -37,6 +39,11 @@ cv::Mat read_image(std::istream & input, bool untie_input)
             if (newline_count == 3 && buf[1] != '4') {
                 if (buf[1] == '6') {
                     bit_per_pixel = 24;
+                } else {
+                    std::stringstream ss(std::string(buf.begin() + second_newline_idx, buf.end()));
+                    size_t t;
+                    ss >> t;
+                    bit_per_pixel = std::ceil(std::log2(t));
                 }
                 reading_prefix = false;
             }
